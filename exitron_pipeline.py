ffb69322e1026c68f71ef02d6e85831cd6ecf7c8
KalynaLab <teamkalyna@gmail.com>
2020-03-16 12:07:25 +0100

    Added BAM index file assertion

--- exitron_pipeline.py
+++ exitron_pipeline.py
@@ -307,7 +307,7 @@ def calculate_PSI(work_dir, exitron_info, quant_mode, bam_file, file_handle, NPR
 	from multiprocessing import Pool
 
 	# Make sure the BAM file is indexed
-	assert any([ os.path.exists(bam_file.replace('bam', 'bai'), os.path.exists(bam_file+'.bai') ]),"BAM index file missing. Please run \"samtools index <bam_file>\" and try again".
+	assert any([ os.path.exists(bam_file.replace('bam', 'bai'), os.path.exists(bam_file+'.bai') ]),"BAM index file missing. Please run \"samtools index <bam_file>\" and try again."
 
     # Read exitron info
 	rc, info = {}, {}
